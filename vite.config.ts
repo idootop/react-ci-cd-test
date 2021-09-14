31a6207e71f4781c28b9f630fcb6de1fc07661f4
WJG <wjg12341234@163.com>
2021-09-14 15:52:37 +0800

    修正vite打包base路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: '/react-ci-cd-test/',
   build: {
     sourcemap: true,
   },
